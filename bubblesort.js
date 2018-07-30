9d45cbfd28cfa3b102a2de841f294ae730bb0edc
Jasmine English <thisjasmine0@gmail.com>
2018-07-30 15:11:46 -0400

    Driver: Jasmine

--- bubblesort.js
+++ bubblesort.js
@@ -1,17 +1,45 @@
 
+let swapCount = 0
+
 function bubbleSort(array){
+    //let outputArray = [];
+    
+    if (array.length <= 1){
+        return array
+    }
 
-}
+    //let swapCount = 0
+    for(var i = 0; i < array.length - 1; i++) {
+        let elem1 = array[i];
+        let elem2 = array[i+1];
+        if (compare(elem1,elem2)){
+            swap(elem1,elem2, array, i)
+        }
+        
+    }
 
-function compare(item1, item2){
-    let temp1 = item1
-    let temp2 = item2
-    if (temp1 > temp2){
-        item2 = temp1
-        item1 = temp2
+    if (swapCount === 0){
+        return array
+    }
+    else {
+        swapCount = 0
+        return bubbleSort(array)
     }
 }
 
-function swap(){
-    
+function compare(elem1, elem2){
+    if (elem1 > elem2){
+        return true
+    }
+    return false
+}
+
+function swap(elem1, elem2,array,i){
+    //let swapCount = 0
+    function swapThese () {
+        array[i] = elem2
+        array[i+1] = elem1
+        swapCount++
+    }
+    swapThese()
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,8 +1,5 @@
 describe('Bubble Sort', function() {
 
-    spyOn(swap())
-    expect(swap()).toHaveBeenCalled()
-    spyOn(/* */) //comparison count
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([])
   })
@@ -12,10 +9,22 @@ describe('Bubble Sort', function() {
   })
 
   it('handles multiple items', function() {
-    expect(bubbleSort([1,3,2])).toEqual([1,2,3])
+    expect(bubbleSort([1,3,2,5,8,0,4,6])).toEqual([0,1,2,3,4,5,6,8])
   })
 
-  
+  it('check that swap function is called', function() {
+    spyOn(window, 'swap').and.callThrough()
+    bubbleSort([1,3,2])
+    expect(swap.calls.count()).toEqual(1);
+  })
+
+  it('check that compare function is called', function() {
+
+    spyOn(window, 'compare').and.callThrough()
+    bubbleSort([4,6,5,1])
+    expect(compare.calls.count()).toEqual(10);
+    })
+
 })
 
 // describe('A suite is just a function', function() {
